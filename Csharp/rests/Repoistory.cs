29a8f875666b525aaf5a7e424d26c7181a703023
Gabrial <493686444@qq.com>
2021-04-19 17:30:11 +0800

    静态和实例    第一题

--- Csharp/rests/Repoistory.cs
+++ Csharp/rests/Repoistory.cs
@@ -9,7 +9,10 @@ namespace Csharp
     /// </summary>
     class Repoistory
     {
+        //1.一个int类型的常量version
+        //2.一个静态只读的字符串connection，以后可用于连接数据库
+        //3.思考Respoitory应该是static类还是实例类更好
         public int version;
-        static string connection;
+        public static string connection { private set; get; }
     }
 }
