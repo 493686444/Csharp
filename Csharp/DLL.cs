5a611a9949cfe3e814752fec37c24d81153bb6e5
Gabrial <493686444@qq.com>
2021-04-22 12:14:09 +0800

    双向链表完成

---
+++ Csharp/DLL.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csharp
+{
+    //双向链表(Doubly Linked List)
+    public class DLL
+    {
+        #region 数据
+        public int ID;
+        public DLL left;
+        public DLL right;
+
+        #endregion
+
+        //方法可精简(将同者取出),但精简后不易理解
+        #region 功能---加
+        public void Addleft(DLL target)
+        {
+            if (this.left == null)
+            { this.left = target; }
+            else
+            {
+                target.right = this;
+                target.left = this.left;
+
+                this.left.right = target;
+                this.left = target;
+            }
+        }
+        public void Addright(DLL target)
+        {
+            if (this.right == null)
+            { this.right = target; }
+            else
+            {
+                target.left = this;
+                target.right = this.right;
+
+                this.right.left = target;
+                this.right = target;
+            }
+        }
+
+
+
+        #endregion
+
+        #region 功能---减
+        public void Delete()
+        {
+            //if (right==null&&left==null)//左右两个null
+            //{
+            //    //这种情况不存在,链表  最少要有两个
+            //}
+            if (left == null && right != null)//  在左有一个null
+            { right.left = null; right = null; }
+            else if (left != null && right == null)// 在右有一个null
+            { left.right = null; left = null; }
+            else//无null
+            {
+                right.left = left;
+                left.right = right;
+            }
+        }
+        #endregion
+
+        #region 功能---移动
+        public static void Swap(DLL theright, DLL theleft)//两个对象之间交换 静态更好
+        {
+            //思路一 : 利用加减做,  思路二 : 备份6个对象  思路三 : 面向过程做
+
+            //这个代码没有完善  左右连在一起的情况没写
+
+            //备份
+            DLL theleftup = theleft, therightup = theright;
+            //修改右槽
+            theright.left.right = theleft;
+            if (theright.right != null)
+            { theright.right.left = theleft; }
+            //else {  }
+            
+            //修改左槽
+            theleft.right.left = theright;
+            if (theleft.left != null)
+            { theleft.right.left = theright;  }
+            //else { }
+
+            //修改左者
+            DLL backup_left = theleft;  //备份
+            theleft.left = theright.left;
+            if (theright.right!=null)
+            { theleft.right = theright.right; }
+            else
+            { theleft.right = null; }
+            //修改右者
+            theright.right = backup_left.right;
+            if (backup_left.left != null)
+            { theright.left = backup_left.left; }
+            else
+            { theright.left = null; }
+
+
+
+        }
+        //想写个ChangeTo的方法( 属于对象 但是多余,还是算了)
+        #endregion
+
+        //[选] FindBy()：根据节点值查找到某个节点  //没懂是啥意思
+
+
+
+        //自 : 问1 :为啥没这个对象自己呢?  答 : 因为 这是一个类(模板),{ 模板就是他自己的模板 },
+        //      当模板实例化之后就是他自己了,所以没必要加他自己,这里涉及到变量和对象的关系.
+        //问2 :对象和变量有啥关系  答 : 变量是用来直接或者间接 存放对象的 , 有时候会直接放个值或者字符串,这个是和new一下等价的
+
+
+
+
+    }
+}
--- MyTest/UnitTest1.cs
+++ MyTest/UnitTest1.cs
@@ -4,44 +4,87 @@ using System;
 
 namespace MyTest
 {
+    public class DLLTest
+    {
+        //测试的公用数据放在这里
+        DLL node1 = new DLL(); DLL node2 = new DLL(); DLL node3 = new DLL();
+        DLL node4 = new DLL(); DLL node5 = new DLL();
+
+        [SetUp]//这里对公用数据进行加工处理
+        public void Setup()   //为什么不在这里直接设置字段   因为方法内的数据不公用(否则也就不会出现返回值这个概念了)
+        {
+            node1.right = node2; node2.right = node3; node3.right = node4; node4.right = node5;
+            node5.left = node4; node4.left = node3; node3.left = node2; node2.left = node1;
+        }
+
+        [Test]//加最左边
+        public void AddleftTest()
+        {
+            DLL node1_L = new DLL();
+            node1.Addleft(node1_L);
+            DLL expection = node1.left;
+            Assert.AreEqual(expection, node1_L);
+        }
+        [Test]//加左边
+        public void AddleftTest2()
+        {
+            DLL node2_L = new DLL();
+            node2.Addleft(node2_L);
+            DLL expection1 = node2.left;
+            Assert.AreEqual(expection1, node2_L);
+            DLL expection2 = node1.right;
+            Assert.AreEqual(expection2, node2_L);
+        }
+
+        [Test]
+        public void DeleteTest()
+        {
+            node1.Delete();//最边边
+            Assert.AreEqual(null, node2.left);
+            node3.Delete();//中间
+            Assert.AreEqual(node2, node4.left);
+            Assert.AreEqual(node4, node2.right);
+        }
+        [Test]
+        public void Swap()
+        {
+            DLL.Swap(node2, node4);
+            Assert.AreEqual(node2, node5.left);
+            Assert.AreEqual(node4, node3.right);
+        }
+
+    }
     public class Tests
     {
-        
-        [SetUp]
-        public void Setup()
-        {
-            
-        }
 
         [Test]//1.数组中找到最小值
         public void GetMinTest()
         {
             double[] scores = { 1, 2, 3, 4, 5, };
-            double numbers= procedure.GetMin(scores);
-            Assert.AreEqual(1,numbers);  // Assert.AreEqual(期望, student.Age);
+            double numbers = procedure.GetMin(scores);
+            Assert.AreEqual(1, numbers);  // Assert.AreEqual(期望, student.Age);
         }
 
         [Test]//2.找到100以内的所有质数
         public void PrimenumberTest()
         {
-            int[] expectations = new int[25] 
+            int[] expectations = new int[25]
             { 2, 3, 5, 7, 11, 13, 17,19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
-            int[] numbers= procedure.Primenumber(100);
+            int[] numbers = procedure.Primenumber(100);
             Assert.AreEqual(expectations, numbers);
         }
 
         [Test]//3.猜数字游戏
         public void GuessMeTest()
         {
-         //暂时搁置
+            //暂时搁置
         }
 
         [Test]//4. 二分查找
         public void BinarySeekTest()
         {
-            int[] numbers = new int[10] { 0,1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            int expection = 
-            procedure.BinarySeek(numbers, 3);
+            int[] numbers = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int expection = procedure.BinarySeek(numbers, 3);
             Assert.AreEqual(4, expection);
         }
 
@@ -61,17 +104,6 @@ namespace MyTest
             mystack.Push(3);
             Assert.AreEqual(null, mystack.Push(4));
         }
-
-
-
-
-        //继续完成双向链表的测试和开发，实现：
-
-        //node3.InsertAfter(node1); 场景
-        //InerstBefore()：在某个节点前插入
-        //Delete()：删除某个节点
-        //[选] Swap()：交互某两个节点
-        //[选] FindBy()：根据节点值查找到某个节点
 
     }
 }
