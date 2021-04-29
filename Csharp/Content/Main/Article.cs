5d00c7a4704353d2b9592dacde5cd92e1968c84f
Gabrial <493686444@qq.com>
2021-04-29 17:24:41 +0800

    小修改

--- Csharp/Content/Main/Article.cs
+++ Csharp/Content/Main/Article.cs
@@ -37,17 +37,17 @@ namespace Csharp
         {
             _publishTime = DateTime.Now;
 
-        }
-        #endregion
-
-        public void Change()
-        {
             if (Author is null)
             {
                 Console.WriteLine("作者参数不能为空");
                 throw new ArgumentNullException();//2.内容（Content）发布（Publish）的时候检查其作者（Author）是否为空，如果为空抛出“参数为空”异常
             }
             else { }
+        }
+        #endregion
+
+        public void Change()
+        {
         }
 
 
