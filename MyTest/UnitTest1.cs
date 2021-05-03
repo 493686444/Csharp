701c0b87b6047bf40c357e91923613c0254357ab
Gabrial <493686444@qq.com>
2021-05-03 08:19:33 +0800

    验证码完成

--- MyTest/UnitTest1.cs
+++ MyTest/UnitTest1.cs
@@ -1,118 +1,118 @@
-using Csharp;
-using NUnit.Framework;
-using System;
+//using Csharp;
+//using NUnit.Framework;
+//using System;
 
-namespace MyTest
-{
-    public class DLLTests
-    {
-        //测试的公用数据放在这里
-        DLL node1 = new DLL(); DLL node2 = new DLL(); DLL node3 = new DLL();
-        DLL node4 = new DLL(); DLL node5 = new DLL();
+//namespace MyTest
+//{
+//    public class DLLTests
+//    {
+//        //测试的公用数据放在这里
+//        DLL node1 = new DLL(); DLL node2 = new DLL(); DLL node3 = new DLL();
+//        DLL node4 = new DLL(); DLL node5 = new DLL();
 
-        [SetUp]//这里对公用数据进行加工处理
-        public void Setup()   //为什么不在这里直接设置字段   因为方法内的数据不公用(否则也就不会出现返回值这个概念了)
-        {
-            node1.left = null;node5.right = null;
-            node1.right = node2; node2.right = node3; node3.right = node4; node4.right = node5;
-            node5.left = node4; node4.left = node3; node3.left = node2; node2.left = node1;
-        }
+//        [SetUp]//这里对公用数据进行加工处理
+//        public void Setup()   //为什么不在这里直接设置字段   因为方法内的数据不公用(否则也就不会出现返回值这个概念了)
+//        {
+//            node1.left = null;node5.right = null;
+//            node1.right = node2; node2.right = node3; node3.right = node4; node4.right = node5;
+//            node5.left = node4; node4.left = node3; node3.left = node2; node2.left = node1;
+//        }
 
-        [Test]//加最左边
-        public void AddleftTest()
-        {
-            DLL node1_L = new DLL();
-            node1.Addleft(node1_L);
-            DLL expection = node1.left;
-            Assert.AreEqual(expection, node1_L);
-        }
-        [Test]//加左边
-        public void AddleftTest2()
-        {
-            DLL node2_L = new DLL();
-            node2.Addleft(node2_L);
-            DLL expection1 = node2.left;
-            Assert.AreEqual(expection1, node2_L);
-            DLL expection2 = node1.right;
-            Assert.AreEqual(expection2, node2_L);
-        }
+//        [Test]//加最左边
+//        public void AddleftTest()
+//        {
+//            DLL node1_L = new DLL();
+//            node1.Addleft(node1_L);
+//            DLL expection = node1.left;
+//            Assert.AreEqual(expection, node1_L);
+//        }
+//        [Test]//加左边
+//        public void AddleftTest2()
+//        {
+//            DLL node2_L = new DLL();
+//            node2.Addleft(node2_L);
+//            DLL expection1 = node2.left;
+//            Assert.AreEqual(expection1, node2_L);
+//            DLL expection2 = node1.right;
+//            Assert.AreEqual(expection2, node2_L);
+//        }
 
-        [Test]
-        public void DeleteTest()
-        {
-            node1.Delete();//最边边
-            Assert.AreEqual(null, node2.left);//有时候红有时候绿...... 因为跑得是上一次的build
-            node3.Delete();//中间
-            Assert.AreEqual(node2, node4.left);
-            Assert.AreEqual(node4, node2.right);
-        }
-        [Test]
-        public void Swap()
-        {
-            DLL.Swap(node1, node2);
-            Assert.AreEqual(node1,node2.right);
-            //Assert.IsNull(node2.left);//不懂为啥这个过不来
-        }
-        [Test]
-        public void Swap2()
-        {
-            DLL.Swap(node2, node4);
-            Assert.AreEqual(node2, node5.left);
-            Assert.AreEqual(node2, node3.right);
-        }
+//        [Test]
+//        public void DeleteTest()
+//        {
+//            node1.Delete();//最边边
+//            Assert.AreEqual(null, node2.left);//有时候红有时候绿...... 因为跑得是上一次的build
+//            node3.Delete();//中间
+//            Assert.AreEqual(node2, node4.left);
+//            Assert.AreEqual(node4, node2.right);
+//        }
+//        [Test]
+//        public void Swap()
+//        {
+//            DLL.Swap(node1, node2);
+//            Assert.AreEqual(node1,node2.right);
+//            //Assert.IsNull(node2.left);//不懂为啥这个过不来
+//        }
+//        [Test]
+//        public void Swap2()
+//        {
+//            DLL.Swap(node2, node4);
+//            Assert.AreEqual(node2, node5.left);
+//            Assert.AreEqual(node2, node3.right);
+//        }
 
 
-    }
-    public class Tests
-    {
+//    }
+//    public class Tests
+//    {
 
-        [Test]//1.数组中找到最小值
-        public void GetMinTest()
-        {
-            double[] scores = { 1, 2, 3, 4, 5, };
-            double numbers = procedure.GetMin(scores);
-            Assert.AreEqual(1, numbers);  // Assert.AreEqual(期望, student.Age);
-        }
+//        [Test]//1.数组中找到最小值
+//        public void GetMinTest()
+//        {
+//            double[] scores = { 1, 2, 3, 4, 5, };
+//            double numbers = procedure.GetMin(scores);
+//            Assert.AreEqual(1, numbers);  // Assert.AreEqual(期望, student.Age);
+//        }
 
-        [Test]//2.找到100以内的所有质数
-        public void PrimenumberTest()
-        {
-            int[] expectations = new int[25]
-            { 2, 3, 5, 7, 11, 13, 17,19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
-            int[] numbers = procedure.Primenumber(100);
-            Assert.AreEqual(expectations, numbers);
-        }
+//        [Test]//2.找到100以内的所有质数
+//        public void PrimenumberTest()
+//        {
+//            int[] expectations = new int[25]
+//            { 2, 3, 5, 7, 11, 13, 17,19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
+//            int[] numbers = procedure.Primenumber(100);
+//            Assert.AreEqual(expectations, numbers);
+//        }
 
-        [Test]//3.猜数字游戏
-        public void GuessMeTest()
-        {
-            //暂时搁置
-        }
+//        [Test]//3.猜数字游戏
+//        public void GuessMeTest()
+//        {
+//            //暂时搁置
+//        }
 
-        [Test]//4. 二分查找
-        public void BinarySeekTest()
-        {
-            int[] numbers = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            int expection = procedure.BinarySeek(numbers, 3);
-            Assert.AreEqual(4, expection);
-        }
+//        [Test]//4. 二分查找
+//        public void BinarySeekTest()
+//        {
+//            int[] numbers = new int[10] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//            int expection = procedure.BinarySeek(numbers, 3);
+//            Assert.AreEqual(4, expection);
+//        }
 
-        [Test]//5.栈的压入弹出
-        public void Test()
-        {
-            MimicStack<int> mystack = new MimicStack<int>(3);
-            //栈空
-            Assert.AreEqual(false, mystack.Pop());//开始以为这括号(assert的括号)里的代码不会对外面产生影响,但实际上确实产生嘞
-            //入栈
-            Assert.AreEqual(1, mystack.Push(1));
-            //出站
-            Assert.AreEqual(true, mystack.Pop());
-            //溢出
-            mystack.Push(1);
-            mystack.Push(2);
-            mystack.Push(3);
-            Assert.AreEqual(null, mystack.Push(4));
-        }
+//        [Test]//5.栈的压入弹出
+//        public void Test()
+//        {
+//            MimicStack<int> mystack = new MimicStack<int>(3);
+//            //栈空
+//            Assert.AreEqual(false, mystack.Pop());//开始以为这括号(assert的括号)里的代码不会对外面产生影响,但实际上确实产生嘞
+//            //入栈
+//            Assert.AreEqual(1, mystack.Push(1));
+//            //出站
+//            Assert.AreEqual(true, mystack.Pop());
+//            //溢出
+//            mystack.Push(1);
+//            mystack.Push(2);
+//            mystack.Push(3);
+//            Assert.AreEqual(null, mystack.Push(4));
+//        }
 
-    }
-}
+//    }
+//}
--- captcha/Program.cs
+++ captcha/Program.cs
@@ -1,26 +1,98 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace captcha
 {
+
     class Program
     {
+     
         static void Main(string[] args)
         {
             //IO和文件操作
 
-            //一.现有一个txt文件，里面存放了若干email地址，使用分号（;）或者换行进行了分隔。请删除其中重复的email地址，并按每30个email一行（行内用; 分隔）重新组织
-         
+            // 参考一起帮的登录页面，绘制一个验证码图片，存放到当前项目中。验证码应包含：
+            {
+                //1.随机字符串
+                //2.混淆用的各色像素点
+                //3.混淆用的直线（或曲线）
+            }
+            string[] letter = new string[] { "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m" };
+
+            //随机数
+            Random random = new Random();
+    
+
+            Bitmap board = new Bitmap(200, 80);//画板
+
+            Graphics g = Graphics.FromImage(board);//图
+            g.Clear(Color.FromArgb
+                    (
+                        random.Next(255),
+                        random.Next(255),
+                        random.Next(255),
+                        random.Next(255)
+                        ));
+
+            //乱线和点
+            for (int i = 0; i < 100; i++)
+            {
+
+                g.DrawLine(
+                    new Pen(
+                        Color.FromArgb
+                    (
+                        random.Next(255),
+                        random.Next(255),
+                        random.Next(255),
+                        random.Next(255)
+                        )
+                    ),
+                    new Point(random.Next(200), random.Next(80)),
+                    new Point(random.Next(200), random.Next(80)));
+                board.SetPixel(random.Next(200), random.Next(80), Color.FromArgb
+                    (
+                        random.Next(255),
+                        random.Next(255),
+                        random.Next(255),
+                        random.Next(255)
+                        ));
+
+            }
+
+            //字母
+            g.DrawString(
+               letter[random.Next(0, 25)] + letter[random.Next(0, 25)] + letter[random.Next(0, 25)] + letter[random.Next(0, 25)],
+               new Font("Tahoma", 55),
+               new SolidBrush(Color.FromArgb
+                    (
+                        random.Next(255),
+                        random.Next(255),
+                        random.Next(255),
+                        random.Next(255)
+                        )),
+               new PointF(1, 0)
+               );
+
+
+            //保存的位置
+            board.Save(@"E:\hello.jpg");
+
+
+            //一.现有一个txt文件，里面存放了若干email地址，使用分号（;）或者换行进行了分隔。
+            //请删除其中重复的email地址，并按每30个email一行（行内用; 分隔）重新组织
+
+
             //二.将生成验证码的代码拆分成若干个方法，并为其添加异常机制，要求能够：
-            { 
+            {
                 //1.显式的抛出一个自定义异常
                 //2.捕获并包裹一个被抛出的异常，记入日志文件，然后再次抛出
                 //3.根据不同的异常，给用户相应的友好的异常提示
                 //4.使用using释放文件资源
-            }   
+            }
 
         }
     }
